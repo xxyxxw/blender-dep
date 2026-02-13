b4f899c7e9b308cb4b02206e05ec552907e0b2eb
Yuto Watanabe <w.yuto928@gmail.com>
2026-02-13 23:34:43 +0900

    Update Supabase API key

--- public/js/supabase.js
+++ public/js/supabase.js
@@ -4,7 +4,7 @@
 // Project Settings > API > Project URL / anon public key
 
 const SUPABASE_URL = "https://fwtrvwelfjuxwfgjglvk.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ3dHJ2d2VsZmp1eHdmZ2pnbHZrIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3MDk4MjM3MywiZXhwIjoyMDg2NTU4MzczfQ.KrDKTGBg_LycodM_MybwbA0m207KTnpOzoooHg0PJmU";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ3dHJ2d2VsZmp1eHdmZ2pnbHZrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA5ODIzNzMsImV4cCI6MjA4NjU1ODM3M30.Nax2jf3zaQX6iFViGwTPrYJ4bZTk3D3XwOcm-hAjU04";
 
 // Supabaseクライアントの作成 (CDNのグローバル変数 'supabase' を使用)
 // 注意: index.htmlでCDNを読み込んでいる必要があります
